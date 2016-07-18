565bfc6aad7ff54344d695278e5834a24d7701d7
Vincentas Vienozinskis <vincnetas@gmail.com>
2016-07-18 16:26:18 +0300

    sample data

---
+++ src/main/resources/data-hsqldb.sql
@@ -0,0 +1,12 @@
+INSERT INTO user_account (id, admin, display_name, email, google_id, phone, tag_id, valid_till) VALUES (151, true, 'User', 'email@gmail.com', '103797420082588414881', NULL, '111111111', '2016-08-29 00:00:00');
+
+INSERT INTO access_log (id, date_time, user_id) VALUES (223, '2016-07-08 15:54:39.263', 151);
+INSERT INTO access_log (id, date_time, user_id) VALUES (224, '2016-07-08 15:54:41.548', 151);
+INSERT INTO access_log (id, date_time, user_id) VALUES (225, '2016-07-08 15:54:46.953', 151);
+INSERT INTO access_log (id, date_time, user_id) VALUES (226, '2016-07-08 15:54:49.215', 151);
+INSERT INTO access_log (id, date_time, user_id) VALUES (227, '2016-07-08 15:54:51.445', 151);
+INSERT INTO access_log (id, date_time, user_id) VALUES (228, '2016-07-08 15:54:54.748', 151);
+INSERT INTO access_log (id, date_time, user_id) VALUES (229, '2016-07-08 22:59:36.249', 151);
+INSERT INTO access_log (id, date_time, user_id) VALUES (230, '2016-07-08 23:05:53.195', 151);
+INSERT INTO access_log (id, date_time, user_id) VALUES (231, '2016-07-08 16:09:15.019', 151);
+INSERT INTO access_log (id, date_time, user_id) VALUES (232, '2016-07-08 16:11:50.446', 151);
